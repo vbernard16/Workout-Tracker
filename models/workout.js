f3ece1f4cc13edb5815341cb47f0e6786d67ce95
vb <vaughnbernard16@gmail.com>
2023-01-30 08:30:26 -0500

    removed days field from workout model

--- models/workout.js
+++ models/workout.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
 const routineSchema = require('./routine')
-// possibly rename routine
+
 const workoutSchema = new mongoose.Schema({
-    // add virtuals and options: timestamp
-    // for new date, do not include specific time, or GMT
+
     name: {
         type: String,
         default: 'Workout: ' + new Date
     },
-    days: String,
     routines: [routineSchema],
     owner: {
         type: mongoose.Schema.Types.ObjectId,
